4e9c8bf374e111ab4d02ee0b1a9f8dd0fba44c82
Alok Debnath <alokdebnath.in@gmail.com>
2023-10-28 20:01:08 +0530

    ui changes

--- src/app/wfh_mail/page.jsx
+++ src/app/wfh_mail/page.jsx
@@ -7,6 +7,8 @@ const EmailGenerator = () => {
   const [emailSubject, setEmailSubject] = useState('');
   const [emailBody, setEmailBody] = useState('');
 
+  const [additionalTime, setAdditionalTime] = useState(0);
+
   const templateOptions = Object.keys(templateData);
 
   const handleTemplateChange = (event) => {
@@ -23,15 +25,27 @@ const EmailGenerator = () => {
       year: 'numeric',
     });
 
-    const time = new Date().toLocaleString('en-US', {
+    const currentTime = new Date();
+
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+
+    currentTime.setMinutes(minutes + additionalTime);
+
+    const time = currentTime.toLocaleString('en-US', {
       hour: '2-digit',
       minute: '2-digit',
       hour12: true,
     });
+
     const formattedDate = date.replace(/(\d+)\/(\d+)\/(\d+)/, '$2/$1/$3');
 
+    const modifiedEmailBody = template.body
+      .replace('{time}', time)
+      .replace('{additionalTime}', additionalTime);
+
     setEmailSubject(template.subject.replace('{date}', formattedDate).replace('{time}', time));
-    setEmailBody(template.body.replace('{time}', time));
+    setEmailBody(modifiedEmailBody);
   };
 
   return (
@@ -54,14 +68,47 @@ const EmailGenerator = () => {
                   </option>
                 ))}
               </select>
-              <div
-                className='textarea textarea-bordered'
-                dangerouslySetInnerHTML={{ __html: emailSubject }}
-              />
-              <div
-                className='textarea textarea-bordered'
-                dangerouslySetInnerHTML={{ __html: emailBody }}
-              />
+              <div className='grid grid-cols-3 gap-4 place-content-between'>
+                <input
+                  type='radio'
+                  aria-label='0'
+                  name='extraTime'
+                  className='btn'
+                  onClick={() => {
+                    setAdditionalTime(0);
+                  }}
+                />
+                <input
+                  type='radio'
+                  aria-label='+1'
+                  name='extraTime'
+                  className='btn'
+                  onClick={() => {
+                    setAdditionalTime(1);
+                  }}
+                />
+                <input
+                  type='radio'
+                  aria-label='+2'
+                  name='extraTime'
+                  className='btn'
+                  onClick={() => {
+                    setAdditionalTime(2);
+                  }}
+                />
+              </div>
+              <div className='mockup-code mb-3 mt-2'>
+                <div className='px-5'>
+                  <div
+                    className='textarea textarea-bordered'
+                    dangerouslySetInnerHTML={{ __html: emailSubject }}
+                  />
+                  <div
+                    className='textarea textarea-bordered'
+                    dangerouslySetInnerHTML={{ __html: emailBody }}
+                  />
+                </div>
+              </div>
               <button
                 type='submit'
                 className='btn'>
