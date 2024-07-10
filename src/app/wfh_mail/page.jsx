af911aa5e6ff1f1c62a94de25c3e287091eb6f4a
Alok Debnath <alokdebnath.in@gmail.com>
2024-07-10 12:10:59 +0000

    initial implementation of dynamic to and cc

--- src/app/wfh_mail/page.jsx
+++ src/app/wfh_mail/page.jsx
@@ -1,12 +1,15 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { templateData } from '@/constants/WFHemailTemplate';
-import { templateSignature } from '@/constants/WFHemailTemplate';
 
 const EmailGenerator = () => {
   const [selectedTemplate, setSelectedTemplate] = useState('loginMail');
   const [emailSubject, setEmailSubject] = useState('');
   const [emailBody, setEmailBody] = useState('');
+  const [additionalFields, setAdditionalFields] = useState({
+    to: '',
+    cc: '',
+  });
 
   const [additionalTime, setAdditionalTime] = useState(0);
 
@@ -46,11 +49,40 @@ const EmailGenerator = () => {
       .replace('{additionalTime}', additionalTime);
 
     setEmailSubject(template.subject.replace('{date}', formattedDate).replace('{time}', time));
-    setEmailBody(modifiedEmailBody + templateSignature.body);
+    setEmailBody(modifiedEmailBody);
+  };
+
+  useEffect(() => {
+    const WFH_email_to = localStorage.getItem('WFH_email_to') || '';
+    const WFH_email_cc = localStorage.getItem('WFH_email_cc') || '';
+
+    setAdditionalFields({
+      to: WFH_email_to,
+      cc: WFH_email_cc,
+    });
+  }, []);
+
+  const handleEmailClick = () => {
+    if (!additionalFields.to) {
+      return;
+    }
+
+    localStorage.setItem('WFH_email_to', additionalFields.to);
+    localStorage.setItem('WFH_email_cc', additionalFields.cc);
+
+    const subject = emailSubject;
+    const body = emailBody;
+
+    const mailtoLink = `mailto:${additionalFields.to}?${
+      additionalFields.cc ? `cc=${additionalFields.cc}&` : ''
+    }subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    window.open(mailtoLink, '_blank');
   };
 
   return (
-    <div className='hero min-h-screen bg-base-200'>
+    <div
+      className='hero min-h-screen bg-base-200'
+      data-theme='night'>
       <div className='hero-content'>
         <div className='max-w-md'>
           <div className='card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
@@ -100,22 +132,96 @@ const EmailGenerator = () => {
               </div>
               <div className='mockup-code mb-3 mt-2'>
                 <div className='px-5'>
-                  <div
-                    className='textarea textarea-bordered'
-                    dangerouslySetInnerHTML={{ __html: emailSubject }}
-                  />
-                  <div
-                    className='textarea textarea-bordered'
-                    dangerouslySetInnerHTML={{ __html: emailBody }}
-                  />
+                  <div className='textarea textarea-bordered'>
+                    <p
+                      className='font-small text-base-content'
+                      dangerouslySetInnerHTML={{ __html: emailSubject }}
+                    />
+                  </div>
+                  <div className='textarea textarea-bordered'>
+                    <p
+                      className='font-small text-base-content'
+                      dangerouslySetInnerHTML={{ __html: emailBody.replace(/\n/g, '<br>') }}
+                    />
+                  </div>
                 </div>
               </div>
               <button
                 type='submit'
                 className='btn'>
-                Regenerate Email
+                {emailBody ? 're' : ''}generate Email
               </button>
+              {emailBody ? (
+                <button
+                  type='button'
+                  className='btn btn-success'
+                  onClick={() => document.getElementById('my_modal_1').showModal()}>
+                  Next
+                </button>
+              ) : null}
             </form>
+            <dialog
+              id='my_modal_1'
+              className='modal modal-bottom sm:modal-middle'>
+              <div className='modal-box'>
+                <div className='space-y-4'>
+                  <div className='form-control w-full'>
+                    <div className='label'>
+                      <span className='label-text'>To:</span>
+                    </div>
+                    <input
+                      type='text'
+                      placeholder='Type here'
+                      value={additionalFields.to}
+                      className='input input-bordered w-full'
+                      onChange={(e) =>
+                        setAdditionalFields((prevState) => ({
+                          ...prevState,
+                          to: e.target.value,
+                        }))
+                      }
+                    />
+                  </div>
+                  <div className='form-control w-full'>
+                    <div className='label flex'>
+                      <span className='label-text'>CC:</span>
+                      <span className='badge badge-info'>Optional</span>
+                    </div>
+                    <input
+                      type='text'
+                      placeholder='Type here'
+                      value={additionalFields.cc}
+                      className='input input-bordered w-full'
+                      onChange={(e) =>
+                        setAdditionalFields((prevState) => ({
+                          ...prevState,
+                          cc: e.target.value,
+                        }))
+                      }
+                    />
+                  </div>
+                </div>
+                <div className='modal-action'>
+                  <div className='join flex w-full'>
+                    <form
+                      className='join-item flex-1'
+                      method='dialog'>
+                      <button className='btn w-full rounded-e-none'>Back</button>
+                    </form>
+                    <span
+                      className='btn btn-primary join-item flex-1'
+                      onClick={handleEmailClick}>
+                      Send Email
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <form
+                method='dialog'
+                className='modal-backdrop'>
+                <button>close</button>
+              </form>
+            </dialog>
           </div>
         </div>
       </div>
--- src/constants/WFHemailTemplate.js
+++ src/constants/WFHemailTemplate.js
@@ -2,40 +2,11 @@ export const templateData = {
   loginMail: {
     name: 'WFH login mail',
     subject: 'Log-in Time ({date}) {time}',
-    body: `
-        Hi HR,
-        <br />
-        Logged in at {time}.
-      `,
+    body: `Hi HR,\nLogged in at {time}.`,
   },
   logoutMail: {
     name: 'WFH logout mail',
     subject: 'Logged-out Time ({date}) {time}',
-    body: `
-        Hi HR,
-        <br />
-        Logged out at {time}.
-      `,
+    body: `Hi HR,\nLogged out at {time}.`,
   },
-};
-
-export const templateSignature = {
-  body: `
-      <font color='#888888'>
-        <br />
-        <br />
-        --
-        <div>
-          <i>Regards,</i>
-        </div>
-        <div>
-          <font color='#9e9e9e'>
-            <b>Alok Kumar</b>
-          </font>
-        </div>
-        <div>
-          <span style='color:#9e9e9eff'>PHP Developer</span>
-        </div>
-      </font>
-  `,
 };
